4e49bdac0328c170a33b0799cb2411a0f466eba0
Vanessa <yvvergaraortiz@gmail.com>
2020-08-18 22:00:43 -0500

    Ejercicio1
    Primer ejercicio

---
+++ EjerciciosVanessaVergaraC#/Ejercicios/Ejercicio1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios
+{
+    class Ejercicio1
+    {
+        
+        static void Main(string[] args)
+        {
+            int variable1 = 0;
+            int variable2 = 0;
+            int variable3 = 0;
+            int suma = 0;
+            variable1 = int.Parse(Console.ReadLine());
+            variable2 = int.Parse(Console.ReadLine());
+            variable3 = int.Parse(Console.ReadLine());
+            suma = variable1 + variable2 + variable3;
+            Console.WriteLine(suma);
+            Console.ReadKey();
+
+        }
+    }
+}
